c1c7ff730bd63e40b259d84d01ca4b58669abffe
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-04-14 01:56:43 +0530

    added game functionalities

--- main.js
+++ main.js
@@ -2,16 +2,25 @@
 const game = document.querySelector("#gameboard");
 const player = document.querySelector("#player");
 const startcells = ["","","","","","","","",""];
+let turn = "circle";
 player.textContent = " Player 1 goes First";
 function gameElements(){
-    startcells.forEach((cell,index) => {
+    startcells.forEach((_cell,index) => {
     const cellElement = document.createElement('div')
-    cellElement.classList.add("bg-white", "h-full", "rounded-lg")
-    console.log(cellElement)
-    const circle = document.createElement('div')
-    circle.classList.add("h-9", "w-9", "rounded-full", "border-3", "border-blue-400")
+    cellElement.classList.add("bg-white", "h-full", "rounded-lg", "border-t-orange-700","border-b-purple-700","border-l-red-600", "border-r-blue-600","border-4", "flex", "items-center", "justify-center", "box-border")
+    cellElement.id= index;
+    cellElement.addEventListener("click", addsymbol)
     game.append(cellElement)
-    cellElement.append(circle)
     });
 }
 gameElements();
+
+function addsymbol(e){
+    const display = document.createElement("div");
+    display.classList.add(turn)
+    e.target.append(display)
+    turn = turn === "circle" ? "cross": "circle";
+    let current_player = turn === "circle"? " player 1's": " player 2's";
+    player.textContent = "It is now" + current_player +" turn";
+   
+}
