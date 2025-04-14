94051fa97a312ff3824d618ff6d7e31603f26ff9
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-04-14 16:52:30 +0530

    completed player 1 and player 2 game functionality

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 "use strict";
 const game = document.querySelector("#gameboard");
 const player = document.querySelector("#player");
+const startbtn = document.querySelector("#Game_btn");
+const restartbtn = document.querySelector("#Restart");
+restartbtn.classList.add("hidden");
 const startcells = ["","","","","","","","",""];
 let turn = "circle";
-player.textContent = " Player 1 goes First";
-function gameElements(){
+startbtn.addEventListener("click", gameElements)
+function gameElements(e){
+    game.classList.add("gamearea")
     startcells.forEach((_cell,index) => {
     const cellElement = document.createElement('div')
-    cellElement.classList.add("bg-white", "h-full", "rounded-lg", "border-t-orange-700","border-b-purple-700","border-l-red-600", "border-r-blue-600","border-4", "flex", "items-center", "justify-center", "box-border")
+    cellElement.classList.add("square")
     cellElement.id= index;
     cellElement.addEventListener("click", addsymbol)
     game.append(cellElement)
+player.textContent = " Player 1 goes First";
+startbtn.classList.add("hidden");
+restartbtn.classList.remove("hidden");
+restartbtn.addEventListener("click", restartgame)
     });
 }
-gameElements();
+function restartgame(){
+    window.location.reload();
+}
 
 function addsymbol(e){
     const display = document.createElement("div");
@@ -22,5 +32,24 @@ function addsymbol(e){
     turn = turn === "circle" ? "cross": "circle";
     let current_player = turn === "circle"? " player 1's": " player 2's";
     player.textContent = "It is now" + current_player +" turn";
+    e.target.removeEventListener("click", addsymbol);
+    checkscore();
+}
+function checkscore(){
+    const squares = document.querySelectorAll(".square");
+    const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    winningCombos.forEach(arr=>{
+        const circleWins = arr.every(cell=> squares[cell].firstChild?.classList.contains("circle"))
+        const crossWins = arr.every(cell=> squares[cell].firstChild?.classList.contains("cross"))
+        if(circleWins){
+            player.textContent="Player 1 wins!";
+            squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+        }
+        else if(crossWins){
+            player.textContent="Player 2 wins!";
+            squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+        }
+    })
    
 }
